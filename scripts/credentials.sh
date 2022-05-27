1b608c86fb6350c127488834766d2c8ab522db51
Brice Cauchon <brice.cauchon.1@ulaval.ca>
2022-05-27 12:16:01 -0400

    Added the notebook-server and modified credentials.sh

--- scripts/credentials.sh
+++ scripts/credentials.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Grep password values
-CERT_PW=$(sed '1!d' passwords.txt)
-ADMIN_PW=$(sed '2!d' passwords.txt)
-# change the passwords in the files
+CERT_PW="change.it"
+ADMIN_PW="change.it"
+# change the passwords above
 jq --arg passwd "$CERT_PW" '.tomcat.keystore_password = $passwd' arcgis-enterprise-primary.json > tmp.$$.json && mv tmp.$$.json arcgis-enterprise-primary.json
 jq --arg passwd "$ADMIN_PW" '.arcgis.server.admin_password = $passwd' arcgis-enterprise-primary.json > tmp.$$.json && mv tmp.$$.json arcgis-enterprise-primary.json
 jq --arg passwd "$CERT_PW" '.arcgis.server.keystore_password = $passwd' arcgis-enterprise-primary.json > tmp.$$.json && mv tmp.$$.json arcgis-enterprise-primary.json
 jq --arg passwd "$ADMIN_PW" '.arcgis.portal.admin_password = $passwd' arcgis-enterprise-primary.json > tmp.$$.json && mv tmp.$$.json arcgis-enterprise-primary.json
 jq --arg passwd "$CERT_PW" '.arcgis.portal.keystore_password = $passwd' arcgis-enterprise-primary.json > tmp.$$.json && mv tmp.$$.json arcgis-enterprise-primary.json
+jq --arg passwd "$ADMIN_PW" '.arcgis.portal.admin_password = $passwd' notebook-server-federation.json > tmp.$$.json && mv tmp.$$.json notebook-server-federation.json
+jq --arg passwd "$ADMIN_PW" '.arcgis.notebook_server.admin_password = $passwd' notebook-server-federation.json > tmp.$$.json && mv tmp.$$.json notebook-server-federation.json
 # change renewal script
 sed -i "s/change.it/$CERT_PW/" ./scripts/auto_pfx.sh
