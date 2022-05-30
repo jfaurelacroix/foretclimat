23ca16ec6d40ec8e25031a64c0ed09fbb383886b
Brice Cauchon <brice.cauchon.1@ulaval.ca>
2022-05-30 16:11:17 -0400

    Added support for Notebook Server
    Changed install guide and added the correct json to configure Notebook Server

--- scripts/credentials.sh
+++ scripts/credentials.sh
@@ -9,5 +9,8 @@ jq --arg passwd "$ADMIN_PW" '.arcgis.portal.admin_password = $passwd' arcgis-ent
 jq --arg passwd "$CERT_PW" '.arcgis.portal.keystore_password = $passwd' arcgis-enterprise-primary.json > tmp.$$.json && mv tmp.$$.json arcgis-enterprise-primary.json
 jq --arg passwd "$ADMIN_PW" '.arcgis.portal.admin_password = $passwd' notebook-server-federation.json > tmp.$$.json && mv tmp.$$.json notebook-server-federation.json
 jq --arg passwd "$ADMIN_PW" '.arcgis.notebook_server.admin_password = $passwd' notebook-server-federation.json > tmp.$$.json && mv tmp.$$.json notebook-server-federation.json
+jq --arg passwd "$ADMIN_PW" '.arcgis.notebook_server.admin_password = $passwd' notebook-server.json > tmp.$$.json && mv tmp.$$.json notebook-server.json
+jq --arg passwd "$ADMIN_PW" '.arcgis.notebook_server.admin_password = $passwd' arcgis-notebook-server-webadaptor.json > tmp.$$.json && mv tmp.$$.json arcgis-notebook-server-webadaptor.json
+jq --arg passwd "$CERT_PW" '.tomcat.keystore_password = $passwd' arcgis-notebook-server-webadaptor.json > tmp.$$.json && mv tmp.$$.json arcgis-notebook-server-webadaptor.json
 # change renewal script
 sed -i "s/change.it/$CERT_PW/" ./scripts/auto_pfx.sh
