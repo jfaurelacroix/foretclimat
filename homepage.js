ded09828664c9382873612d64f4386b759294c05
Brice Cauchon <brice.cauchon.1@ulaval.ca>
2022-06-09 15:22:27 -0400

    Adding authentication on homepage

--- homepage.js
+++ homepage.js
@@ -1,11 +1,17 @@
 require([
+  "esri/identity/OAuthInfo",
   "esri/config",
   "esri/WebMap",
   "esri/views/MapView",
   "esri/widgets/Search",
   "esri/widgets/BasemapToggle"
-], (esriConfig, WebMap, MapView, Search, BasemapToggle) => {
+], (OAuthInfo, esriConfig, WebMap, MapView, Search, BasemapToggle) => {
   esriConfig.portalUrl = "https://www.foretclimat.ca/portal";
+
+  let auth = new OAuthInfo({
+    portalUrl: esriConfig.portalUrl
+  });
+
   const map = new WebMap({
     portalItem: {
       // autocasts as new PortalItem()
@@ -41,15 +47,31 @@ require([
       e.style.opacity = "1";
     } else {
       e.style.opacity = "0";
-      setTimeout(() => {  e.style.visibility = "hidden"; }, 500);
+      setTimeout(() => {
+        e.style.visibility = "hidden";
+      }, 500);
     }
   });
 
+  document.cookie = 'esri_auth=%7B%22portalApp%22%3Atrue%2C%22email%22%3A%22BRCAU5%40ulaval.ca%22%2C%22token%22%3A%22ONTnG8naWH5sJLyfxZNecZPk7j9ksT-iBAofQEgvFHWpMucD-XL6r5QpxRWUqDcWoeaRsMoMkeH5Nl6q3dfyr4pEeZyM_uWZOgMsl3iv6dr-dOBDhJDZe6VrouNs-SQ2Vq8S1zetNV5BvcjRD2_JZ9_TJDmgcf4C1-8mEDwCaExh78edjgVnYjR4v05ONw5rUw2nLeIx1d0SbacRFJ5LBHu8MDI8ccZqvboGPq5ZEUE.%22%2C%22expires%22%3A1656002922194%2C%22allSSL%22%3Atrue%2C%22persistent%22%3Afalse%2C%22created%22%3A1654798122194%2C%22culture%22%3A%22%22%2C%22region%22%3Anull%2C%22accountId%22%3A%220123456789ABCDEF%22%2C%22role%22%3A%22org_user%22%2C%22customBaseUrl%22%3A%22www.foretclimat.ca%2Fportal%22%7D';
   document.getElementById("login").addEventListener("click", function() {
     var e = document.getElementById("ddlInstitution");
     var strInstitution = e.value;
     window.location.href = "https://www.foretclimat.ca/portal/home/signin.html";
   });
 
+  var thumbnail = "https://www.foretclimat.ca/portal/home/10.9.0/js/arcgisonline/css/images/no-user-thumb.jpg";
+  if (getCookie("esri_auth") != "") {
+    var userJSON = getUserJSON(getCookieEmail());
+    if (userJSON != ""){
+      if(userJSON.thumbnail != null){
+      thumbnail =
+        "https://www.foretclimat.ca/portal/sharing/rest/community/users/" +
+        getCookieEmail() + "/info/" + userJSON.thumbnail;
+      document.getElementById("greeter").innerHTML = "Hello, " + userJSON.fullName + "!";
+      }
+    }
+  }
 
+  document.getElementById("accountThumbnail").src = thumbnail;
 });
--- utils.js
+++ utils.js
@@ -14,3 +14,37 @@ function changeNav() {
     closeNav();
   }
 }
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function getCookieJSON(){
+    return JSON.parse(getCookie("esri_auth"));
+}
+
+function getCookieEmail(){
+    let username = getCookieJSON().email;
+    return username;
+}
+
+function getUserJSON(email){
+    let url = '../sharing/rest/community/users/' + email + '?f=pjson';
+    var answer = "";
+    jQuery.getJSON(url, function(data){
+        answer = data;
+    });
+    return answer;
+}
