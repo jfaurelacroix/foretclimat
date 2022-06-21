84324cb0ad9d53643aec7a4ced285c261f51052f
Brice Cauchon <brice.cauchon.1@ulaval.ca>
2022-06-20 20:17:58 -0400

    Changed index map

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,9 @@ require([
   "esri/widgets/Locate",
   "esri/portal/Portal",
   "esri/layers/FeatureLayer",
-  "esri/intl"
-], (esriConfig, WebMap, MapView, Search, BasemapToggle, Locate, Portal, FeatureLayer, intl) => {
+  "esri/intl",
+  "esri/widgets/LayerList"
+], (esriConfig, WebMap, MapView, Search, BasemapToggle, Locate, Portal, FeatureLayer, intl, LayerList) => {
   esriConfig.portalUrl = "https://www.foretclimat.ca/portal";
   intl.setLocale(locale);
 
@@ -25,7 +26,7 @@ require([
   const map = new WebMap({
     portalItem: {
       // autocasts as new PortalItem()
-      id: "b74d05881ff84f45a1ecb1c7eb17e5b1", // id is in the content page url
+      id: "8dacc82f02d94d24bb4a3c751ba4db34", // id is in the content page url
       portal: myPortal
     }
   });
@@ -35,38 +36,19 @@ require([
     container: "viewDiv"
   });
 
-  const popupQualRebois = {
-    "title": "Reboisement",
-    "content": "<b>ID:</b> {OBJECTID}<br><b>Realise:</b> {REALISE}<br><b>ANNEE:</b> {ANNEE}<br>"
-  }
-
-  const featureQualRebois = new FeatureLayer({
-    url: "https://www.foretclimat.ca/server/rest/services/Index_MIL1/MapServer/0",
-    outFields: ["OBJECTID", "REALISE", "ANNEE"],
-    popupTemplate: popupQualRebois
+  const layerList = new LayerList({
+    view: view
   });
-
-  const popupRegen = {
-    "title": "Regeneration",
-    "content": "<b>ID:</b> {OBJECTID}<br><b>COMMENT:</b> {COMMENT}<br><b>DATE:</b> {DATE}<br>"
-  }
-
-  const featureRegen = new FeatureLayer({
-    url: "https://www.foretclimat.ca/server/rest/services/Index_MIL1/MapServer/1",
-    outFields: ["OBJECTID", "COMMENT", "DATE"],
-    popupTemplate: popupRegen
-  });
-
-  map.add(featureQualRebois);
-  map.add(featureRegen);
-
-
   const search = new Search({
     view: view,
     portal: myPortal, // https://enterprise.arcgis.com/fr/portal/latest/administer/windows/configure-portal-to-geocode-addresses.htm
     sources: [ //https://developers.arcgis.com/javascript/latest/sample-code/widgets-search-multiplesource/
       {
-        layer: featureQualRebois,
+        layer: new FeatureLayer({
+          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/0",
+          outFields: ["*"]
+        }),
+        name: "Points inventaire de matière ligneuse non utilisée",
         exactMatch: false,
         outFields: ["*"],
         maxResults: 6,
@@ -75,7 +57,154 @@ require([
         minSuggestCharacters: 0
       },
       {
-        layer: featureRegen,
+        layer: new FeatureLayer({
+          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/1",
+          outFields: ["*"]
+        }),
+        name: "Polygones inventaire de matière ligneuse non utilisée",
+        exactMatch: false,
+        outFields: ["*"],
+        maxResults: 6,
+        maxSuggestions: 6,
+        suggestionsEnabled: true,
+        minSuggestCharacters: 0
+      },
+      {
+        layer: new FeatureLayer({
+          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/2",
+          outFields: ["*"]
+        }),
+        name: "Points inventaire d'intervention",
+        exactMatch: false,
+        outFields: ["*"],
+        maxResults: 6,
+        maxSuggestions: 6,
+        suggestionsEnabled: true,
+        minSuggestCharacters: 0
+      },
+      {
+        layer: new FeatureLayer({
+          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/3",
+          outFields: ["*"]
+        }),
+        name: "Polygones inventaire d'intervention'",
+        exactMatch: false,
+        outFields: ["*"],
+        maxResults: 6,
+        maxSuggestions: 6,
+        suggestionsEnabled: true,
+        minSuggestCharacters: 0
+      },
+      {
+        layer: new FeatureLayer({
+          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/4",
+          outFields: ["*"]
+        }),
+        name: "Points inventaire de reboisement",
+        exactMatch: false,
+        outFields: ["*"],
+        maxResults: 6,
+        maxSuggestions: 6,
+        suggestionsEnabled: true,
+        minSuggestCharacters: 0
+      },
+      {
+        layer: new FeatureLayer({
+          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/5",
+          outFields: ["*"]
+        }),
+        name: "Polygones inventaire de reboisement",
+        exactMatch: false,
+        outFields: ["*"],
+        maxResults: 6,
+        maxSuggestions: 6,
+        suggestionsEnabled: true,
+        minSuggestCharacters: 0
+      },
+      {
+        layer: new FeatureLayer({
+          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/6",
+          outFields: ["*"]
+        }),
+        name: "Points inventaire de récolte",
+        exactMatch: false,
+        outFields: ["*"],
+        maxResults: 6,
+        maxSuggestions: 6,
+        suggestionsEnabled: true,
+        minSuggestCharacters: 0
+      },
+      {
+        layer: new FeatureLayer({
+          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/8",
+          outFields: ["*"]
+        }),
+        name: "Polygones inventaire de récolte",
+        exactMatch: false,
+        outFields: ["*"],
+        maxResults: 6,
+        maxSuggestions: 6,
+        suggestionsEnabled: true,
+        minSuggestCharacters: 0
+      },
+      {
+        layer: new FeatureLayer({
+          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/7",
+          outFields: ["*"]
+        }),
+        name: "Points inventaire de régénération",
+        exactMatch: false,
+        outFields: ["*"],
+        maxResults: 6,
+        maxSuggestions: 6,
+        suggestionsEnabled: true,
+        minSuggestCharacters: 0
+      },
+      {
+        layer: new FeatureLayer({
+          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/9",
+          outFields: ["*"]
+        }),
+        name: "Polygones inventaire de régénération",
+        exactMatch: false,
+        outFields: ["*"],
+        maxResults: 6,
+        maxSuggestions: 6,
+        suggestionsEnabled: true,
+        minSuggestCharacters: 0
+      },
+      {
+        layer: new FeatureLayer({
+          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/10",
+          outFields: ["*"]
+        }),
+        name: "Points inventaire de suivi de plantation",
+        exactMatch: false,
+        outFields: ["*"],
+        maxResults: 6,
+        maxSuggestions: 6,
+        suggestionsEnabled: true,
+        minSuggestCharacters: 0
+      },
+      {
+        layer: new FeatureLayer({
+          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/12",
+          outFields: ["*"]
+        }),
+        name: "Polygones inventaire de suivi de plantation",
+        exactMatch: false,
+        outFields: ["*"],
+        maxResults: 6,
+        maxSuggestions: 6,
+        suggestionsEnabled: true,
+        minSuggestCharacters: 0
+      },
+      {
+        layer: new FeatureLayer({
+          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/11",
+          outFields: ["*"]
+        }),
+        name: "Secteur",
         exactMatch: false,
         outFields: ["*"],
         maxResults: 6,
@@ -98,7 +227,7 @@ require([
   view.ui.add(["textBoxDiv", search], "top-left");
   // places the search widget in the top right corner of the view
   view.ui.add(["account"], "top-right");
-  view.ui.add([toggle, "partLogoDiv"], "bottom-left");
+  view.ui.add([toggle, "partLogoDiv", layerList], "bottom-left");
   view.ui.add([locateWidget], "bottom-right");
   view.ui.move(["zoom"], "bottom-right");
   
