5c824090c15307212746cd94c37692670a0dd3f3
Brice Cauchon <brice.cauchon.1@ulaval.ca>
2022-06-21 16:48:07 -0400

    added popup templating, legend, layerlist, changed color schemes

--- pages/index.js
+++ pages/index.js
@@ -14,8 +14,11 @@ require([
   "esri/portal/Portal",
   "esri/layers/FeatureLayer",
   "esri/intl",
-  "esri/widgets/LayerList"
-], (esriConfig, WebMap, MapView, Search, BasemapToggle, Locate, Portal, FeatureLayer, intl, LayerList) => {
+  "esri/widgets/LayerList",
+  "esri/widgets/Legend",
+  "esri/Color",
+  "esri/PopupTemplate"
+], (esriConfig, WebMap, MapView, Search, BasemapToggle, Locate, Portal, FeatureLayer, intl, LayerList, Legend, Color, PopupTemplate) => {
   esriConfig.portalUrl = "https://www.foretclimat.ca/portal";
   intl.setLocale(locale);
 
@@ -36,19 +39,361 @@ require([
     container: "viewDiv"
   });
 
-  const layerList = new LayerList({
-    view: view
+
+  const IMLNU_PS = new FeatureLayer({
+    url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/0",
+    outFields: ["*"],
+    title: "ILMNU_POINTS",
+    renderer: {
+      type: "simple", 
+      symbol: {
+        type: "simple-marker",
+        size: 4,
+        color: new Color("rgb(255, 160, 0)")
+      },
+    },
+    popupTemplate: new PopupTemplate({
+      title: "IMLNU",
+      content: [{
+        // Pass in the fields to display
+        type: "fields",
+        fieldInfos: [{
+          fieldName: "objectid",
+          label: "ID"
+        }, {
+          fieldName: "annee",
+          label: "Année"
+       }]
+      }]
+    })
   });
+
+  const IMLNU_BLOC = new FeatureLayer({
+    url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/1",
+    outFields: ["*"],
+    title: "ILMNU_POLYGONES",
+    legendEnabled: false,
+    renderer: {
+      type: "simple", 
+      symbol: {
+        type: "simple-fill",
+        color: new Color("rgba(255, 160, 0, 0.8)")
+      },  // autocasts as new SimpleFillSymbol()
+    },
+    popupTemplate: new PopupTemplate({
+      title: "IMLNU",
+      content: [{
+        type: "media", //https://developers.arcgis.com/javascript/latest/api-reference/esri-popup-content-MediaContent.html
+        // Autocasts as array of MediaInfo objects
+        mediaInfos: [{
+          title: "<b>test</b>",
+          type: "image", // Autocasts as new ImageMediaInfo object
+          // Autocasts as new ImageMediaInfoValue object
+          caption: "test",
+          value: {
+            sourceUrl: "https://www.foretclimat.ca/portal/sharing/rest/content/items/130af69915c24157900dbc915a8963d3/data"
+          }
+        }]
+      }]
+    })
+  });
+
+  const INTER_PS =  new FeatureLayer({
+    url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/2",
+    outFields: ["*"],
+    title: "INTER_POINTS",
+    renderer: {
+      type: "simple", 
+      symbol: {
+        type: "simple-marker",
+        size: 4,
+        color: new Color("rgb(133, 91, 207)")
+      },
+    },
+    popupTemplate: new PopupTemplate({
+      title: "Intervention",
+      content: [{
+        // Pass in the fields to display
+        type: "fields",
+        fieldInfos: [{
+          fieldName: "objectid",
+          label: "ID"
+        }, {
+          fieldName: "annee",
+          label: "Année"
+       }]
+      }]
+    })
+  });
+
+  const INTER_BLOC = new FeatureLayer({
+    url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/3",
+    outFields: ["*"],
+    title: "INTER_POLYGONES",
+    legendEnabled: false,
+    renderer: {
+      type: "simple", 
+      symbol: {
+        type: "simple-fill",
+        color: new Color("rgba(133, 91, 207, 0.8)")
+      },  // autocasts as new SimpleFillSymbol()
+    },
+    popupTemplate: new PopupTemplate({
+      title: "INTER",
+      content: [{
+        type: "media", //https://developers.arcgis.com/javascript/latest/api-reference/esri-popup-content-MediaContent.html
+        activeMediaInfoIndex: 1,
+        // Autocasts as array of MediaInfo objects
+        mediaInfos: [
+        {
+          type: "image", // Autocasts as new ImageMediaInfo object
+          // Autocasts as new ImageMediaInfoValue object
+          value: {
+            sourceUrl: "https://www.foretclimat.ca/portal/sharing/rest/content/items/130af69915c24157900dbc915a8963d3/data"
+          }
+        }]
+      }]
+    })
+  });
+
+  const REBOIS_PS = new FeatureLayer({
+    url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/4",
+    outFields: ["*"],
+    title: "REBOIS_POINTS",
+    renderer: {
+      type: "simple", 
+      symbol: {
+        type: "simple-marker",
+        size: 4,
+        color: new Color("rgb(106, 90, 205)")
+      },
+    },
+    popupTemplate: new PopupTemplate({
+      title: "Reboisement",
+      content: [{
+        // Pass in the fields to display
+        type: "fields",
+        fieldInfos: [{
+          fieldName: "objectid",
+          label: "ID"
+        }, {
+          fieldName: "annee",
+          label: "Année"
+       }]
+      }]
+    })
+  });
+
+  const REBOIS_BLOC = new FeatureLayer({
+    url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/5",
+    outFields: ["*"],
+    title: "REBOIS_POLYGONES",
+    legendEnabled: false,
+    renderer: {
+      type: "simple", 
+      symbol: {
+        type: "simple-fill",
+        color: new Color("rgba(106, 90, 205, 0.8)")
+      },  // autocasts as new SimpleFillSymbol()
+    },
+    popupTemplate: new PopupTemplate({
+      title: "REBOIS",
+      content: [{
+        type: "media", //https://developers.arcgis.com/javascript/latest/api-reference/esri-popup-content-MediaContent.html
+        activeMediaInfoIndex: 1,
+        // Autocasts as array of MediaInfo objects
+        mediaInfos: [
+        {
+          type: "image", // Autocasts as new ImageMediaInfo object
+          // Autocasts as new ImageMediaInfoValue object
+          value: {
+            sourceUrl: "https://www.foretclimat.ca/portal/sharing/rest/content/items/130af69915c24157900dbc915a8963d3/data"
+          }
+        }]
+      }]
+    })
+  });
+
+  const RECOLTE_PS =  new FeatureLayer({
+    url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/6",
+    outFields: ["*"],
+    title: "RECOLTE_POINTS",
+    renderer: {
+      type: "simple", 
+      symbol: {
+        type: "simple-marker",
+        size: 4,
+        color: new Color("rgb(255, 119, 113)")
+      },
+    },
+    popupTemplate: new PopupTemplate({
+      title: "Récolte",
+      content: [{
+        // Pass in the fields to display
+        type: "fields",
+        fieldInfos: [{
+          fieldName: "objectid",
+          label: "ID"
+        }, {
+          fieldName: "annee",
+          label: "Année"
+       }]
+      }]
+    })
+  });
+
+  const RECOLTE_BLOC = new FeatureLayer({
+    url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/8",
+    outFields: ["*"],
+    title: "RECOLTE_POLYGONES",
+    legendEnabled: false,
+    renderer: {
+      type: "simple", 
+      symbol: {
+        type: "simple-fill",
+        color: new Color("rgba(255, 119, 113, 0.8)")
+      },  // autocasts as new SimpleFillSymbol()
+    },
+    popupTemplate: new PopupTemplate({
+      title: "RECOLTE",
+      content: [{
+        type: "media", //https://developers.arcgis.com/javascript/latest/api-reference/esri-popup-content-MediaContent.html
+        activeMediaInfoIndex: 1,
+        // Autocasts as array of MediaInfo objects
+        mediaInfos: [
+        {
+          type: "image", // Autocasts as new ImageMediaInfo object
+          // Autocasts as new ImageMediaInfoValue object
+          value: {
+            sourceUrl: "https://www.foretclimat.ca/portal/sharing/rest/content/items/130af69915c24157900dbc915a8963d3/data"
+          }
+        }]
+      }]
+    })
+  });
+
+  const REGEN_PS = new FeatureLayer({
+    url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/7",
+    outFields: ["*"],
+    title: "REGEN_POINTS",
+    renderer: {
+      type: "simple", 
+      symbol: {
+        type: "simple-marker",
+        size: 4,
+        color: new Color("rgb(0, 131, 165)")
+      },
+    },
+    popupTemplate: new PopupTemplate({
+      title: "Régénération",
+      content: [{
+        // Pass in the fields to display
+        type: "fields",
+        fieldInfos: [{
+          fieldName: "objectid",
+          label: "ID"
+        }, {
+          fieldName: "annee",
+          label: "Année"
+       }]
+      }]
+    })
+  });
+
+  const REGEN_BLOC = new FeatureLayer({
+    url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/9",
+    outFields: ["*"],
+    title: "REGEN_POLYGONES",
+    legendEnabled: false,
+    renderer: {
+      type: "simple", 
+      symbol: {
+        type: "simple-fill",
+        color: new Color("rgba(0, 131, 165, 0.8)")
+      },  // autocasts as new SimpleFillSymbol()
+    },
+    popupTemplate: new PopupTemplate({
+      title: "REGEN",
+      content: [{
+        type: "media", //https://developers.arcgis.com/javascript/latest/api-reference/esri-popup-content-MediaContent.html
+        activeMediaInfoIndex: 1,
+        // Autocasts as array of MediaInfo objects
+        mediaInfos: [
+        {
+          type: "image", // Autocasts as new ImageMediaInfo object
+          // Autocasts as new ImageMediaInfoValue object
+          value: {
+            sourceUrl: "https://www.foretclimat.ca/portal/sharing/rest/content/items/130af69915c24157900dbc915a8963d3/data"
+          }
+        }]
+      }]
+    })
+  });
+
+  const PLANT_PS = new FeatureLayer({
+    url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/10",
+    outFields: ["*"],
+    title: "PLANT_POINTS",
+    renderer: {
+      type: "simple", 
+      symbol: {
+        type: "simple-marker",
+        size: 4,
+        color: new Color("rgb(202, 231, 55)")
+      },
+    },
+    popupTemplate: new PopupTemplate({
+      title: "Suivi plantation",
+      content: [{
+        // Pass in the fields to display
+        type: "fields",
+        fieldInfos: [{
+          fieldName: "objectid",
+          label: "ID"
+        }, {
+          fieldName: "annee",
+          label: "Année"
+       }]
+      }]
+    })
+  });
+
+  const PLANT_BLOC = new FeatureLayer({
+    url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/12",
+    outFields: ["*"],
+    title: "PLANT_POLYGONES",
+    legendEnabled: false,
+    renderer: {
+      type: "simple", 
+      symbol: {
+        type: "simple-fill",
+        color: new Color("rgba(202, 231, 55, 0.8)")
+      },  // autocasts as new SimpleFillSymbol()
+    },
+    popupTemplate: new PopupTemplate({
+      title: "PLANT",
+      content: [{
+        type: "media", //https://developers.arcgis.com/javascript/latest/api-reference/esri-popup-content-MediaContent.html
+        activeMediaInfoIndex: 1,
+        // Autocasts as array of MediaInfo objects
+        mediaInfos: [
+        {
+          type: "image", // Autocasts as new ImageMediaInfo object
+          // Autocasts as new ImageMediaInfoValue object
+          value: {
+            sourceUrl: "https://www.foretclimat.ca/portal/sharing/rest/content/items/130af69915c24157900dbc915a8963d3/data"
+          }
+        }]
+      }]
+    })
+  });
+
   const search = new Search({
     view: view,
     portal: myPortal, // https://enterprise.arcgis.com/fr/portal/latest/administer/windows/configure-portal-to-geocode-addresses.htm
     sources: [ //https://developers.arcgis.com/javascript/latest/sample-code/widgets-search-multiplesource/
       {
-        layer: new FeatureLayer({
-          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/0",
-          outFields: ["*"]
-        }),
-        name: "Points inventaire de matière ligneuse non utilisée",
+        layer: IMLNU_PS,
         exactMatch: false,
         outFields: ["*"],
         maxResults: 6,
@@ -57,11 +402,7 @@ require([
         minSuggestCharacters: 0
       },
       {
-        layer: new FeatureLayer({
-          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/1",
-          outFields: ["*"]
-        }),
-        name: "Polygones inventaire de matière ligneuse non utilisée",
+        layer: IMLNU_BLOC,
         exactMatch: false,
         outFields: ["*"],
         maxResults: 6,
@@ -70,11 +411,7 @@ require([
         minSuggestCharacters: 0
       },
       {
-        layer: new FeatureLayer({
-          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/2",
-          outFields: ["*"]
-        }),
-        name: "Points inventaire d'intervention",
+        layer: INTER_PS,
         exactMatch: false,
         outFields: ["*"],
         maxResults: 6,
@@ -83,11 +420,7 @@ require([
         minSuggestCharacters: 0
       },
       {
-        layer: new FeatureLayer({
-          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/3",
-          outFields: ["*"]
-        }),
-        name: "Polygones inventaire d'intervention'",
+        layer: INTER_BLOC,
         exactMatch: false,
         outFields: ["*"],
         maxResults: 6,
@@ -96,11 +429,7 @@ require([
         minSuggestCharacters: 0
       },
       {
-        layer: new FeatureLayer({
-          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/4",
-          outFields: ["*"]
-        }),
-        name: "Points inventaire de reboisement",
+        layer: REBOIS_PS,
         exactMatch: false,
         outFields: ["*"],
         maxResults: 6,
@@ -109,11 +438,7 @@ require([
         minSuggestCharacters: 0
       },
       {
-        layer: new FeatureLayer({
-          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/5",
-          outFields: ["*"]
-        }),
-        name: "Polygones inventaire de reboisement",
+        layer: REBOIS_BLOC,
         exactMatch: false,
         outFields: ["*"],
         maxResults: 6,
@@ -122,11 +447,7 @@ require([
         minSuggestCharacters: 0
       },
       {
-        layer: new FeatureLayer({
-          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/6",
-          outFields: ["*"]
-        }),
-        name: "Points inventaire de récolte",
+        layer: RECOLTE_PS,
         exactMatch: false,
         outFields: ["*"],
         maxResults: 6,
@@ -135,11 +456,7 @@ require([
         minSuggestCharacters: 0
       },
       {
-        layer: new FeatureLayer({
-          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/8",
-          outFields: ["*"]
-        }),
-        name: "Polygones inventaire de récolte",
+        layer: RECOLTE_BLOC,
         exactMatch: false,
         outFields: ["*"],
         maxResults: 6,
@@ -148,11 +465,7 @@ require([
         minSuggestCharacters: 0
       },
       {
-        layer: new FeatureLayer({
-          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/7",
-          outFields: ["*"]
-        }),
-        name: "Points inventaire de régénération",
+        layer: REGEN_PS,
         exactMatch: false,
         outFields: ["*"],
         maxResults: 6,
@@ -161,11 +474,7 @@ require([
         minSuggestCharacters: 0
       },
       {
-        layer: new FeatureLayer({
-          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/9",
-          outFields: ["*"]
-        }),
-        name: "Polygones inventaire de régénération",
+        layer: REGEN_BLOC,
         exactMatch: false,
         outFields: ["*"],
         maxResults: 6,
@@ -174,11 +483,7 @@ require([
         minSuggestCharacters: 0
       },
       {
-        layer: new FeatureLayer({
-          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/10",
-          outFields: ["*"]
-        }),
-        name: "Points inventaire de suivi de plantation",
+        layer: PLANT_PS,
         exactMatch: false,
         outFields: ["*"],
         maxResults: 6,
@@ -187,24 +492,7 @@ require([
         minSuggestCharacters: 0
       },
       {
-        layer: new FeatureLayer({
-          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/12",
-          outFields: ["*"]
-        }),
-        name: "Polygones inventaire de suivi de plantation",
-        exactMatch: false,
-        outFields: ["*"],
-        maxResults: 6,
-        maxSuggestions: 6,
-        suggestionsEnabled: true,
-        minSuggestCharacters: 0
-      },
-      {
-        layer: new FeatureLayer({
-          url: "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/11",
-          outFields: ["*"]
-        }),
-        name: "Secteur",
+        layer: PLANT_BLOC,
         exactMatch: false,
         outFields: ["*"],
         maxResults: 6,
@@ -224,11 +512,19 @@ require([
     view: view
   });
 
+  const layerList = new LayerList({
+    view: view,
+  });
+
+  const legend = new Legend({
+    view: view
+  })
+  map.addMany([PLANT_BLOC, REGEN_BLOC,  RECOLTE_BLOC, REGEN_BLOC, INTER_BLOC,  IMLNU_BLOC, PLANT_PS, REGEN_PS, RECOLTE_PS, REGEN_PS, INTER_PS, IMLNU_PS]);
   view.ui.add(["textBoxDiv", search], "top-left");
   // places the search widget in the top right corner of the view
   view.ui.add(["account"], "top-right");
   view.ui.add([toggle, "partLogoDiv", layerList], "bottom-left");
-  view.ui.add([locateWidget], "bottom-right");
+  view.ui.add([locateWidget, legend], "bottom-right");
   view.ui.move(["zoom"], "bottom-right");
   
   /*
