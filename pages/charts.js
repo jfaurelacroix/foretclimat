ee77be8cd00703344906cc9f0608fc3b71462a2c
Brice Cauchon <brice.cauchon.1@ulaval.ca>
2022-07-20 17:01:39 -0400

    Added a legend and the possibility to plot multiple series together

--- pages/charts.js
+++ pages/charts.js
@@ -14,8 +14,9 @@ require([
     "dojox/charting/plot2d/Areas",
     "dojox/charting/SimpleTheme",
     "dojox/charting/themes/common",
+    "dojox/charting/widget/SelectableLegend",
     "dojox/charting/axis2d/Default",
-  ], (esriConfig, FeatureTable, View, WebMap, Portal, FeatureLayer, Chart, Bars, Lines, Columns, Scatter, Markers, Areas, SimpleTheme, theme) => {
+  ], (esriConfig, FeatureTable, View, WebMap, Portal, FeatureLayer, Chart, Bars, Lines, Columns, Scatter, Markers, Areas, SimpleTheme, theme, SelectableLegend) => {
     esriConfig.portalUrl = "https://www.foretclimat.ca/portal";
 
     const myPortal = new Portal({
@@ -38,18 +39,21 @@ require([
 
     const layerBaseUrl = "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/"
 
-    let displayedTable;
+    
+    var displayedTable;
 
-    let displayedLayer;
+    var displayedLayer;
 
-    let featureLayer = (selectedId) => {
+    /* Changes to and returns the selected FeatureLayer */
+    var featureLayer = (selectedId) => {
         displayedLayer = new FeatureLayer({
             url: layerBaseUrl + selectedId
         })
         return displayedLayer;
     }
 
-    let featureTable = (selectedId) => {
+    /* Changes to and returns the selected FeatureTable */
+    var featureTable = (selectedId) => {
         displayedTable = new FeatureTable({
             view: view,
             layer: featureLayer(selectedId),
@@ -60,14 +64,16 @@ require([
     }
     
 
+    /* Changes the displayed table and changes the options for the attribute selections*/
     function changeLayer(){
         let selectedValue = document.getElementById("layerSelect").value;
         document.getElementById("tableContainer").innerHTML = null;
         featureTable(selectedValue);
-        populateAttributes(selectedValue);
+        populateAllAttributes(selectedValue);
     }
     
-    function populateSelect(){
+    /* Adds the options to the select data table */
+    function populateLayer(){
         const selection = document.getElementById("layerSelect");
         let url = "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer?f=pjson";
         jQuery.getJSON(url, function(data){
@@ -82,8 +88,9 @@ require([
         });
     }
 
-    let chart = new Chart("chart");
-    let myTheme = new SimpleTheme({
+    /* Initialize the chart variable (will be overridden each time a graph is created) */
+    var chart = new Chart("chart");
+    var myTheme = new SimpleTheme({
 		colors: [
 			"#A4CE67",
 			"#739363",
@@ -93,40 +100,48 @@ require([
 		]
 	});
     chart.setTheme(myTheme);
-
+    var selectableLegend = new SelectableLegend({chart: chart, outline: true}, "selectableLegend");
+    
+    /* Formats the data to plot with dojox charting */
     function createSerie(features){
-        let mySerie = new Array(features.length);
-        for (let i = 0; i < features.length; i++){
-            mySerie[i] = {
-                x : features[i].attributes[document.getElementById("attributes1").value],
-                y : features[i].attributes[document.getElementById("attributes2").value]
+        let attrYArray = document.getElementsByClassName("attributesY");
+        let mySeries = new Array(attrYArray.length);
+        for (let i = 0; i < attrYArray.length; i++){
+            mySeries[i] = new Array(features.length);
+            for(let k = 0; k < features.length; k++){
+                mySeries[i][k] = {
+                    x : features[k].attributes[document.getElementById("attributesX").value],
+                    y : features[k].attributes[attrYArray[i].value]
+                }
             }
-
         }
-        mySerie.sort(function(a,b){
-            if(a.x < b.x) return -1;
-            if(a.x > b.x) return 1;
-            return 0;
-        })
-        return mySerie;
+        for(const array of mySeries){
+            array.sort(function(a,b){
+                if(a.x < b.x) return -1;
+                if(a.x > b.x) return 1;
+                return 0;
+            })
+        }
+        return mySeries;
     }
 
-    function fieldsValidation(featureSet, chartType){
+    /* Changes fields to integers and returns which integer corresponds to which string */
+    function transformStringsToInt(featureSet){
         let uniqueStringArray = [];
         let labels = [];
         if(document.getElementById("aggregation").value == 'none'){
             if(featureSet.fields[0].type == 'string'){
                 for (let i = 0; i < featureSet.features.length; i++){
-                    let valueOfAttribute = featureSet.features[i].attributes[document.getElementById("attributes1").value];
+                    let valueOfAttribute = featureSet.features[i].attributes[document.getElementById("attributesX").value];
                     if(valueOfAttribute == '' || valueOfAttribute == null){
                         valueOfAttribute = 'NULL'
                     }
                     if(!uniqueStringArray.includes(valueOfAttribute)){
                         uniqueStringArray.push(valueOfAttribute);
                         labels.push({value: uniqueStringArray.length - 1, text: valueOfAttribute});
-                        featureSet.features[i].attributes[document.getElementById("attributes1").value] = uniqueStringArray.length - 1;
+                        featureSet.features[i].attributes[document.getElementById("attributesX").value] = uniqueStringArray.length - 1;
                     }else{
-                        featureSet.features[i].attributes[document.getElementById("attributes1").value] = uniqueStringArray.indexOf(valueOfAttribute );
+                        featureSet.features[i].attributes[document.getElementById("attributesX").value] = uniqueStringArray.indexOf(valueOfAttribute );
                     }
                     
                 }
@@ -134,16 +149,16 @@ require([
         }else{
             if(featureSet.fields[1].type == 'string'){
                 for (let i = 0; i < featureSet.features.length; i++){
-                    let valueOfAttribute = featureSet.features[i].attributes[document.getElementById("attributes1").value];
+                    let valueOfAttribute = featureSet.features[i].attributes[document.getElementById("attributesX").value];
                     if(valueOfAttribute == '' || valueOfAttribute == null){
                         valueOfAttribute = 'NULL'
                     }
                     if(!uniqueStringArray.includes(valueOfAttribute)){
                         uniqueStringArray.push(valueOfAttribute);
                         labels.push({value: uniqueStringArray.length - 1, text: valueOfAttribute});
-                        featureSet.features[i].attributes[document.getElementById("attributes1").value] = uniqueStringArray.length - 1;
+                        featureSet.features[i].attributes[document.getElementById("attributesX").value] = uniqueStringArray.length - 1;
                     }else{
-                        featureSet.features[i].attributes[document.getElementById("attributes1").value] = uniqueStringArray.indexOf(valueOfAttribute );
+                        featureSet.features[i].attributes[document.getElementById("attributesX").value] = uniqueStringArray.indexOf(valueOfAttribute );
                     }
                     
                 }
@@ -152,6 +167,34 @@ require([
         return labels;
     }
 
+    /* Query the data for the chart being created */
+    function queryForChart(){
+        let myQuery;
+        let outArray = [document.getElementsByClassName("attributesY").length]
+        for (let i = 0; i < document.getElementsByClassName("attributesY").length; i++){
+            outArray[i] = document.getElementsByClassName("attributesY")[i].value;
+        }
+        if(document.getElementById("aggregation").value == 'none'){
+            myQuery = {
+                where: "1=1",
+                outFields: [document.getElementById("attributesX").value].concat(outArray)
+            }
+        }else{
+            myQuery = {
+                where: "1=1",
+                groupByFieldsForStatistics: [document.getElementById("attributesX").value],
+                outStatistics: [{
+                        "statisticType": document.getElementById("aggregation").value,
+                        "onStatisticField": outArray, 
+                        "outStatisticFieldName": outArray
+                    },
+                ]
+            }
+        }
+        return displayedLayer.queryFeatures(myQuery)
+    }
+
+    /* Creates a new chart from the parameters */
     function createChart(type){
         chart.destroy();
         chart = new Chart("chart");
@@ -159,116 +202,117 @@ require([
         chart.addPlot("default",{
             // Add the chart type
             type: type,
+            gap: 20,
             hAxis: "x",
-            vAvis: "y",
-            gap: 20
+            vAxis: "y"
         });
 
-       
-        let myQuery;
-        if(document.getElementById("aggregation").value == 'none'){
-            myQuery = {
-                where: "1=1",
-                outFields: [document.getElementById("attributes1").value, document.getElementById("attributes2").value]
-            }
-        }else{
-            myQuery = {
-                where: "1=1",
-                groupByFieldsForStatistics: [document.getElementById("attributes1").value],
-                outStatistics: [{
-                        "statisticType": document.getElementById("aggregation").value,
-                        "onStatisticField": document.getElementById("attributes2").value, 
-                        "outStatisticFieldName": document.getElementById("attributes2").value
-                    },
-                ]
-            }
-        }
-        displayedLayer.queryFeatures(myQuery).then((featureSet) => {
+        queryForChart().then((featureSet) => {
             try{
-                let xAxisLabels = fieldsValidation(featureSet, type);
+                let xAxisLabels = transformStringsToInt(featureSet);
                 let myData = createSerie(featureSet.features);
                 let isUsingLabels = true;
                 if(xAxisLabels.length == 0) {
-                    xAxisLabels = null
+                    xAxisLabels = null;
                     isUsingLabels = false;
                 }; 
                 switch(type)
                 {
                     case "Lines":
-                        chart.addAxis("x", {title: document.getElementById("attributes1").value, titleOrientation: "away", labels: xAxisLabels, minorTicks:!isUsingLabels, majorTicks:!isUsingLabels});
-                        chart.addAxis("y", {title: document.getElementById("attributes2").value, vertical: true});
+                        chart.addAxis("x", {title: document.getElementById("attributesX").value, titleOrientation: "away", labels: xAxisLabels, minorTicks:!isUsingLabels, majorTicks:!isUsingLabels});
+                        chart.addAxis("y", {vertical: true});
                         break;
                     case "Columns":
-                        chart.addAxis("x", {title: document.getElementById("attributes1").value, titleOrientation: "away", labels: xAxisLabels, minorTicks:!isUsingLabels, majorTicks:!isUsingLabels});
-                        chart.addAxis("y", {title: document.getElementById("attributes2").value, vertical: true, min:0});
+                        chart.addAxis("x", {title: document.getElementById("attributesX").value, titleOrientation: "away", labels: xAxisLabels, minorTicks:!isUsingLabels, majorTicks:!isUsingLabels});
+                        chart.addAxis("y", { vertical: true, min:0});
                         break;
                     /*case "Bars":
-                        chart.addAxis("x", {title: document.getElementById("attributes1").value, titleOrientation: "away", min:0, });
-                        chart.addAxis("y", {title: document.getElementById("attributes2").value, vertical: true});
                         break;
                     */
                     case "Scatter":
-                        chart.addAxis("x", {title: document.getElementById("attributes1").value, titleOrientation: "away", labels: xAxisLabels, minorTicks:!isUsingLabels, majorTicks:!isUsingLabels});
-                        chart.addAxis("y", {title: document.getElementById("attributes2").value, vertical: true});
+                        chart.addAxis("x", {title: document.getElementById("attributesX").value, titleOrientation: "away", labels: xAxisLabels, minorTicks:!isUsingLabels, majorTicks:!isUsingLabels});
+                        chart.addAxis("y", {vertical: true});
                         break;
                     case "Markers":
-                        chart.addAxis("x", {title: document.getElementById("attributes1").value, titleOrientation: "away", labels: xAxisLabels, minorTicks:!isUsingLabels, majorTicks:!isUsingLabels});
-                        chart.addAxis("y", {title: document.getElementById("attributes2").value, vertical: true});
+                        chart.addAxis("x", {title: document.getElementById("attributesX").value, titleOrientation: "away", labels: xAxisLabels, minorTicks:!isUsingLabels, majorTicks:!isUsingLabels});
+                        chart.addAxis("y", {vertical: true});                    
                         break;
                     case "Areas":
-                        chart.addAxis("x", {title: document.getElementById("attributes1").value, titleOrientation: "away", labels: xAxisLabels, minorTicks:!isUsingLabels, majorTicks:!isUsingLabels});
-                        chart.addAxis("y", {title: document.getElementById("attributes2").value, vertical: true, min:0});
-                        break;
+                        chart.addAxis("x", {title: document.getElementById("attributesX").value, titleOrientation: "away", labels: xAxisLabels, minorTicks:!isUsingLabels, majorTicks:!isUsingLabels});
+                        chart.addAxis("y", {vertical: true, min:0});
                 }
-                chart.addSeries("data", myData);
+                let index = 1
+                for(let el of myData){
+                    chart.addSeries("Serie"+index, el);
+                    index += 1;
+                }
                 chart.render();
+                selectableLegend.chart = chart;
+                selectableLegend.refresh();
             } catch (err){
-                document.getElementById("chart").innerHTML = err;
+                document.getElementById("chart").innerHTML = err.stack;
             }
             
         })
     }
     
+    /* Validates that it can create a graph and creates it */
     function displayChart(){
         let type = document.getElementById("type").value;
+        // Field validation
         try {
-            if(document.getElementById("attributes1").value == document.getElementById("attributes2").value) throw "Impossible. Même attribut";
+            for(let el of document.getElementsByClassName("attributesY")){
+                if(document.getElementById("attributesX").value == el.value) throw "Impossible. Même attribut";
+            }
             createChart(type);
         } catch(err) {
-            document.getElementById("chart").innerHTML = err;
+            document.getElementById("chart").innerHTML = err.stack;
         }
         
     }
 
-    function populateAttributes(selectedValue){
-        let attr1 = document.getElementById("attributes1");
-        let attr2 = document.getElementById("attributes2");
-        attr1.innerHTML = null;
-        attr2.innerHTML = null;
+    /* Adds the options to the attribute selectors */
+    function populateAttribute(selectedValue, selector){
+        selector.innerHTML = null;
         let url = "https://www.foretclimat.ca/server/rest/services/Hosted/BD_Inventaires_Secteur_gdb/FeatureServer/"+ selectedValue +"?f=pjson";
         jQuery.getJSON(url, function(data){
             for (let i = 0; i < data.fields.length; i++){
-                attr1.innerHTML += "<option value='"+ data.fields[i].name +"'>"+ data.fields[i].name +"</option>";
-                attr2.innerHTML += "<option value='"+ data.fields[i].name +"'>"+ data.fields[i].name +"</option>";
+                selector.innerHTML += "<option value='"+ data.fields[i].name +"'>"+ data.fields[i].name +"</option>";         
             }
         });
     }
 
+    function populateAllAttributes(selectedValue){
+    populateAttribute(selectedValue, document.getElementById("attributesX"))
+    for(let el of document.getElementsByClassName("attributesY")){
+        populateAttribute(selectedValue, el);
+    }
+    }
+
+    function addY(){
+        document.getElementById("YAttributesDiv").innerHTML += '<a class="attrYString">Y<sub>' + (document.getElementsByClassName("attributesY").length + 1) + '</sub>:</a><select class="attributesY esri-select"></select> '
+        console.log(document.getElementsByClassName("attributesY")[document.getElementsByClassName("attributesY").length - 1])
+        populateAttribute(document.getElementById("layerSelect").value, document.getElementsByClassName("attributesY")[document.getElementsByClassName("attributesY").length - 1]);
+    }
+
+
     featureTable("13")
-    populateAttributes("13")
-    populateSelect();
+    populateAllAttributes("13");
+    populateLayer();
     document.getElementById("layerSelectButton").addEventListener("click", changeLayer);
-    document.getElementById("plotChart").addEventListener("click", displayChart);
-    let aggrSelect = document.getElementById("aggregation");
+    document.getElementById("plotChartButton").addEventListener("click", displayChart);
+    document.getElementById("addYButton").addEventListener("click", addY);
+    let aggrSelect = document.getElementById("aggregationSelect");
     aggrSelect.addEventListener("change", function() {
         if(aggrSelect.value == 'none'){
-            document.getElementById("attr1String").innerHTML = 'X:';
-            document.getElementById("attr2String").innerHTML = 'Y:';
+            document.getElementById("attrXString").innerHTML = 'X:';
+            document.getElementById("YAttributesDiv").innerHTML = '<a class="attrYString">Y<sub>1</sub>:</a><select class="attributesY esri-select"></select>';
         }else{
-            document.getElementById("attr1String").innerHTML = 'Group by:';
-            document.getElementById("attr2String").innerHTML = aggrSelect.options[aggrSelect.selectedIndex].innerHTML + " attribute:";
+            document.getElementById("attrXString").innerHTML = 'Group by:';
+            document.getElementById("YAttributesDiv").innerHTML = '<a class="attrYString">'+ aggrSelect.options[aggrSelect.selectedIndex].innerHTML + ' attribute:</a><select class="attributesY" class="esri-select"></select>';
         }
+        populateAllAttributes(document.getElementById("layerSelect").value);
     });
-    });
+});
 
 
