d9784020eaf3e9176ff71632ede3a322788092de
Brice Cauchon <70396080+Brice72@users.noreply.github.com>
2023-05-16 13:44:23 -0400

    Update credentials.sh

--- scripts/credentials.sh
+++ scripts/credentials.sh
@@ -41,19 +41,20 @@ prompt_enc_pw
 
 # change the passwords above
 jq --arg passwd "$CERT_PW" '.tomcat.keystore_password = $passwd' arcgis-enterprise-primary.json > tmp.$$.json
-jq --arg passwd "$ADMIN_PW" '.arcgis.server.admin_password = $passwd' tmp.$$.json > tmp.$$.json
-jq --arg passwd "$CERT_PW" '.arcgis.server.keystore_password = $passwd' tmp.$$.json > tmp.$$.json
-jq --arg passwd "$ADMIN_PW" '.arcgis.portal.admin_password = $passwd' tmp.$$.json > tmp.$$.json 
-jq --arg passwd "$CERT_PW" '.arcgis.portal.keystore_password = $passwd' tmp.$$.json > tmp.$$.json 
+jq --arg passwd "$ADMIN_PW" '.arcgis.server.admin_password = $passwd' tmp.$$.json > tmp2.$$.json && mv tmp2.$$.json tmp.$$.json
+jq --arg passwd "$CERT_PW" '.arcgis.server.keystore_password = $passwd' tmp.$$.json > tmp2.$$.json && mv tmp2.$$.json tmp.$$.json
+jq --arg passwd "$ADMIN_PW" '.arcgis.portal.admin_password = $passwd' tmp.$$.json > tmp2.$$.json && mv tmp2.$$.json tmp.$$.json 
+jq --arg passwd "$CERT_PW" '.arcgis.portal.keystore_password = $passwd' tmp.$$.json > tmp2.$$.json && mv tmp2.$$.json tmp.$$.json 
 openssl enc -aes-256-cbc -pass pass:"$ENC_PW" -salt -in tmp.$$.json -out arcgis-enterprise-primary.enc -pbkdf2
 jq --arg passwd "$ADMIN_PW" '.arcgis.portal.admin_password = $passwd' notebook-server-federation.json > tmp.$$.json
-jq --arg passwd "$ADMIN_PW" '.arcgis.notebook_server.admin_password = $passwd' tmp.$$.json > tmp.$$.json 
+jq --arg passwd "$ADMIN_PW" '.arcgis.notebook_server.admin_password = $passwd' tmp.$$.json > tmp2.$$.json && mv tmp2.$$.json tmp.$$.json 
 openssl enc -aes-256-cbc -pass pass:"$ENC_PW" -salt -in tmp.$$.json -out notebook-server-federation.enc -pbkdf2
 jq --arg passwd "$ADMIN_PW" '.arcgis.notebook_server.admin_password = $passwd' notebook-server.json > tmp.$$.json
 openssl enc -aes-256-cbc -pass pass:"$ENC_PW" -salt -in tmp.$$.json -out notebook-server.enc -pbkdf2
 jq --arg passwd "$ADMIN_PW" '.arcgis.notebook_server.admin_password = $passwd' arcgis-notebook-server-webadaptor.json > tmp.$$.json
-jq --arg passwd "$CERT_PW" '.tomcat.keystore_password = $passwd' tmp.$$.json > tmp.$$.json 
+jq --arg passwd "$CERT_PW" '.tomcat.keystore_password = $passwd' tmp.$$.json > tmp2.$$.json && mv tmp2.$$.json tmp.$$.json
 openssl enc -aes-256-cbc -pass pass:"$ENC_PW" -salt -in tmp.$$.json -out arcgis-notebook-server-webadaptor.enc -pbkdf2
 rm tmp.$$.json
+rm tmp2.$$.json
 # change renewal script
 # sed -i "s/change.it/$CERT_PW/" ~/repos/foretclimat/scripts/auto_pfx.sh
