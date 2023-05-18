75505b82a8ca3fa2af17bf277dfd5929ecbd6f5f
Brice Cauchon <70396080+Brice72@users.noreply.github.com>
2023-05-18 10:46:21 -0400

    script renewal non encrypted

--- scripts/auto_pfx.sh
+++ scripts/auto_pfx.sh
@@ -2,22 +2,11 @@
 # Taken from https://github.com/StormWindStudios/OpenSSL-Notes/blob/master/letsencrypt_autopfx.md
 # Adjust these variables as necessary
 
-prompt_cert_pw() {
-    read -s -p "Enter the password for the certificate: " CERT_PW
-	echo
-	read -s -p "Confirm your password: " CONFIRM_PW
-	echo
-
-    if [[ "$CERT_PW" != "$CONFIRM_PW" ]]; then
-        echo "Passwords do not match. Please try again."
-        prompt_cert_pw
-    fi
-}
-
 # Where you want to final PKCS12 file to be stored.
 CERT_PATH="/opt/tomcat_arcgis/cert.pfx"
 
-prompt_cert_pw
+# Password to encrypt the PKCS12 file.
+CERT_PW="change.it"
 
 # Path to LE files, RENEWED_LINEAGE provided by CertBot
 PRIV_KEY_PEM="$RENEWED_LINEAGE/privkey.pem"
@@ -36,3 +25,4 @@ sudo openssl pkcs12 -export -out $CERT_PATH -inkey $PRIV_KEY_PEM -in $CERT_PEM -
 sudo chown arcgis /opt/tomcat_arcgis/cert.pfx
 sudo chmod 700 /opt/tomcat_arcgis/cert.pfx
 echo 'ssl certificate updated at /opt/tomcat_arcgis/cert.pfx'
+echo 'It needs to be updated by running the chef script'
